289d1018bd2d955665c896b5c0b055866eaf80fe
ValentinaNovarino <valenovarino@gmail.com>
2021-01-15 17:29:26 +0100

    stampato a schermo proprietà

--- index.php
+++ index.php
@@ -1,33 +1,50 @@
+<?php
 // definire una classe ‘Movie’
 // all'interno della classe dichiarare delle variabili d'istanza
 // all'interno della classe definire un costruttore
 // all'interno della classe definire almeno un metodo
 // istanziare almeno due oggetti ‘Movie’ e stampare a schermo i valori delle relative proprietà
 
-<?php
 class Movie {
     public $titolo;
     public $regista;
     public $genere;
-    public $year;
+    public $anno;
 
     // definito costruttore
-    function __construct($_titolo, $_genere) {
+    function __construct($_titolo, $_regista) {
         $this->titolo = $_titolo;
-        $this ->genere = $_genere;
+        $this->regista = $_regista;
+    }
+
+    public function getTitolo() {
+        return $this->titolo;
+    }
+
+    public function getRegista() {
+        return $this->regista;
     }
 }
 
 // definito oggetto 1
-$titanic = new Movie('titanic', 'drammatico');
-$titanic->regista = 'James Cameron' ;
-$titanic->year = 1997;
+$titanic = new Movie('Titanic', 'James Cameron');
 
-// definito oggetto 2
-$perlHarbor = new Movie('perl harbor', 'storico');
-$perlHarbor->regista = 'michael bay';
-$perlHarbor->year = 2001;
 
+// // stampo a schermo valori prorpietà film 1
+echo 'FILM 1'.'<br>';
+echo $titanic->getTitolo().'<br>';
+echo $titanic->getRegista().'<br>';
+echo $titanic->genere = 'Drammatico'.'<br>';
+echo $titanic->anno = '1997'.'<br>';
+
+
+// // definito oggetto 2
+$perlHarbor = new Movie('Perl Harbor', 'Michael Bay');
+echo 'FILM 2'.'<br>';
+echo $perlHarbor->getTitolo().'<br>';
+echo $perlHarbor->getRegista().'<br>';
+echo $perlHarbor->genere = 'Storico'.'<br>';
+echo $perlHarbor->anno = '2001';
 
 
 ?>
