956fe12f144e6c276967a45564bd8e228dc84e54
ValentinaNovarino <valenovarino@gmail.com>
2021-01-18 09:57:04 +0100

    separato assegnazioni da echo

--- index.php
+++ index.php
@@ -28,23 +28,25 @@ class Movie {
 
 // definito oggetto 1
 $titanic = new Movie('Titanic', 'James Cameron');
-
-
 // // stampo a schermo valori prorpietà film 1
 echo 'FILM 1'.'<br>';
 echo $titanic->getTitolo().'<br>';
 echo $titanic->getRegista().'<br>';
-echo $titanic->genere = 'Drammatico'.'<br>';
-echo $titanic->anno = '1997'.'<br>';
+$titanic->genere = 'Drammatico';
+echo $titanic->genere.'<br>';
+$titanic->anno = '1997';
+echo $titanic->anno.'<br>';
 
 
 // // definito oggetto 2
 $perlHarbor = new Movie('Perl Harbor', 'Michael Bay');
+// // stampo a schermo valori prorpietà film 2
 echo 'FILM 2'.'<br>';
 echo $perlHarbor->getTitolo().'<br>';
 echo $perlHarbor->getRegista().'<br>';
-echo $perlHarbor->genere = 'Storico'.'<br>';
-echo $perlHarbor->anno = '2001';
-
+$perlHarbor->genere = 'Storico';
+echo $perlHarbor->genere.'<br>';
+$perlHarbor->anno = '2001';
+echo $perlHarbor->anno;
 
 ?>
